043db5382bb72b227b60c62d56d31598d57df717
MarlonChi <marlonchiodelli@hotmail.com>
2025-03-04 21:26:08 -0300

    feat: create user

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,38 @@
 import { Request, Response } from "express";
+import { hash } from "bcrypt";
+import { z } from "zod";
+
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 
 export class UsersController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+
+    if (userWithSameEmail) {
+      throw new AppError("User with same email already exists");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.status(201).json(userWithoutPassword);
   }
 }
